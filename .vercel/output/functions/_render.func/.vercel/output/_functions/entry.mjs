dbd0f04b6c58b9ad59c835d309fb550ce03a35b2
FERDI123456789 <ferdi0909@protonmail.ch>
2024-10-28 00:35:07 -0700

    Live Now

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,25 +1,29 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_njHFt96X.mjs';
-import { manifest } from './manifest_DbdvT8nx.mjs';
+import { manifest } from './manifest_Baa7j9YN.mjs';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
 const _page1 = () => import('./pages/authors/_slug_.astro.mjs');
 const _page2 = () => import('./pages/contact.astro.mjs');
-const _page3 = () => import('./pages/homes/_slug_.astro.mjs');
-const _page4 = () => import('./pages/homes.astro.mjs');
-const _page5 = () => import('./pages/team/_slug_.astro.mjs');
-const _page6 = () => import('./pages/team.astro.mjs');
-const _page7 = () => import('./pages/index.astro.mjs');
+const _page3 = () => import('./pages/contacts/author/_slug_.astro.mjs');
+const _page4 = () => import('./pages/contacts/homes/_slug_.astro.mjs');
+const _page5 = () => import('./pages/homes/_slug_.astro.mjs');
+const _page6 = () => import('./pages/homes.astro.mjs');
+const _page7 = () => import('./pages/team/_slug_.astro.mjs');
+const _page8 = () => import('./pages/team.astro.mjs');
+const _page9 = () => import('./pages/index.astro.mjs');
 
 const pageMap = new Map([
     ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
     ["src/pages/authors/[slug].astro", _page1],
     ["src/pages/contact.astro", _page2],
-    ["src/pages/homes/[slug].astro", _page3],
-    ["src/pages/homes/index.astro", _page4],
-    ["src/pages/team/[slug].astro", _page5],
-    ["src/pages/team/index.astro", _page6],
-    ["src/pages/index.astro", _page7]
+    ["src/pages/contacts/author/[slug].astro", _page3],
+    ["src/pages/contacts/homes/[slug].astro", _page4],
+    ["src/pages/homes/[slug].astro", _page5],
+    ["src/pages/homes/index.astro", _page6],
+    ["src/pages/team/[slug].astro", _page7],
+    ["src/pages/team/index.astro", _page8],
+    ["src/pages/index.astro", _page9]
 ]);
 const serverIslandMap = new Map();
 const _manifest = Object.assign(manifest, {
@@ -29,7 +33,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "6ede1864-1753-4eca-b6e3-b0668d870595",
+    "middlewareSecret": "e8dcee2a-39b8-4023-bcb7-07d4415dff4c",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
